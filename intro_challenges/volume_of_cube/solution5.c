c12301421bee2b3fd8172e6c1a94f3d2bb207ebf
Javad Ibrahimli <cenabibrahimov@gmail.com>
2022-05-29 19:29:08 +0400

    Create solution5.c

---
+++ intro_challenges/volume_of_cube/solution5.c
@@ -0,0 +1,19 @@
+// Replace ___ with your code
+#include<stdio.h>
+
+int main() {
+
+    int length;
+	
+    // take input value for length
+    scanf("%d", &length);
+
+    // compute the volume
+    int volume;
+    volume = length * length * length;
+
+    // print the volume
+    printf("%d", volume );
+
+    return 0;
+}
