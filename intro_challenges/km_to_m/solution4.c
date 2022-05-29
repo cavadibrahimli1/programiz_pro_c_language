8de12f92e712d47e407fe998866d779e34073f52
Javad Ibrahimli <cenabibrahimov@gmail.com>
2022-05-29 19:21:59 +0400

    Create solution4.c

---
+++ intro_challenges/km_to_m/solution4.c
@@ -0,0 +1,19 @@
+// Replace ___ with your code
+#include <stdio.h>
+
+int main() {
+
+    double km;
+    
+    // get input value for km
+    scanf("%lf", &km);
+
+    // convert kilometer to miles
+    double miles;
+    miles = km / 1.6; 
+
+    // print the distance in miles
+    printf("%lf", miles);
+	
+    return 0;
+}
