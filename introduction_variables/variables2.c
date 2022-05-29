1a57a453cdac03d299a22b1da6298e61e6875789
Javad Ibrahimli <cenabibrahimov@gmail.com>
2022-05-29 05:20:27 +0400

    Create variables2.c

---
+++ introduction_variables/variables2.c
@@ -0,0 +1,16 @@
+// Replace ___ with your code
+
+#include <stdio.h>
+
+int main() {
+
+    // create double and character variables
+    double number = 86.99;
+    char letter = 'z';
+
+
+    // print variables
+    printf("%lf  %c", number, letter);
+
+    return 0;
+}
