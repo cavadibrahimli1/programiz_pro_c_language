16b2ecfbe86602ef38feb526b5c7035810eb9e89
Javad Ibrahimli <cenabibrahimov@gmail.com>
2022-05-29 19:08:59 +0400

    Create solution.c

---
+++ intro_challenges/area_of_rectangle/solution.c
@@ -0,0 +1,21 @@
+// Replace ___ with your code
+#include <stdio.h>
+
+int main() {
+    int length;
+    int breadth;
+
+    // get length and breadth input
+    scanf("%d", &length);
+    scanf("%d", &breadth);
+
+    // find the area
+    int area;
+    area = length * breadth;
+
+    // print the area
+    printf("%d", area);
+
+
+    return 0;
+}
