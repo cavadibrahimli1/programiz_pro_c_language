ea77dbb0a7e44d24376a2a0e0816458b230eb051
Javad Ibrahimli <cenabibrahimov@gmail.com>
2022-05-29 19:18:25 +0400

    Create solution3.c

---
+++ intro_challenges/faranheit_to_celcius/solution3.c
@@ -0,0 +1,17 @@
+// Replace ___ with your code
+#include <stdio.h>
+
+int main() {
+    double fahrenheit;
+    
+    // take double input for fahrenheit
+    scanf("%lf", &fahrenheit);
+
+    // convert fahrenheit temperature to celsius
+    double celsius = (fahrenheit - 32) * 5 / 9 ;
+
+    // print the celsius temperature
+    printf("%lf", celsius);
+
+    return 0;
+}
