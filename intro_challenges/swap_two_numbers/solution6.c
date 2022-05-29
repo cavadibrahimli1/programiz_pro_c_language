23d5957827ddac1306f11fdfa87062ef754ac0c8
Javad Ibrahimli <cenabibrahimov@gmail.com>
2022-05-29 19:36:58 +0400

    Create solution6.c

---
+++ intro_challenges/swap_two_numbers/solution6.c
@@ -0,0 +1,37 @@
+
+# Swap Two Numbers
+
+
+#### Easy
+## Problem Description
+
+#### Create a program to swap two numbers using a temporary variable.
+
+The formula to calculate the volume of a cube is
+
+
+
+
+
+
+- Get integer inputs for variables x and y.
+- Use a temporary variable to swap the values of x and y.
+- Print the values of x and y respectively after swapping.
+
+#### Input format:
+An integer
+```bash
+  Input:
+  32
+  67
+```
+
+```bash
+  Output
+  67
+  32
+
+```
+
+
+
